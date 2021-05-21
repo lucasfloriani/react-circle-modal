950b0c20228b570eccaca84f8f732530c4927886
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2021-05-20 22:16:59 -0300

    Updates

---
+++ precache-manifest.cd2c420bc45284e090d60d30c5298179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8a9be16a1ceb04669092e97735e9d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "544625e0b4eef09388ae",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "53c14dda979a8b2a2d4f",
+    "url": "./static/js/2.6af073a1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.6af073a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "544625e0b4eef09388ae",
+    "url": "./static/js/main.24cacdd9.chunk.js"
+  },
+  {
+    "revision": "671e66b393509ef2c1c1",
+    "url": "./static/js/runtime-main.974776b6.js"
+  }
+]);
